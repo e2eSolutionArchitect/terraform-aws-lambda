82f2013b180161eeae71f641dc4c1fb8ee4eab6b
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2024-08-29 19:19:56 -0400

    Automated commit message

--- layer.tf
+++ layer.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_layer_version" "version" {
 	  description      = var.layer_name
 	  s3_bucket        = var.s3_bucket_id
 	  s3_key           = var.s3_layer_key
-	  source_code_hash = var.layer_source_code_hash
+	  #source_code_hash = var.layer_source_code_hash
 	  compatible_runtimes = [var.lambda_runtime]
 }
 
